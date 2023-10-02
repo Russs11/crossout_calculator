59352d7eec37aef7b1d390cc650b87ecfcbdf84b
Russs <russs.khalilov@gmail.com>
2023-10-02 21:00:04 +0300

    h_02.10.2023

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -14,14 +14,14 @@ const Item = ({ id, active, handleClick, img }) => {
 	if (active) {
 		return (
 			<>
-				<div className="component-image component-image-active" style={itemImg}>{id}</div>
+				<div className="component-image component-image-active" style={itemImg}></div>
 			</>
 		);
 	}
 	else {
 		return (
 			<>
-				<div className="component-image " style={itemImg} onClick={() => handleClick(id)}>{id}</div >
+				<div className="component-image " style={itemImg} onClick={() => handleClick(id)}></div >
 			</>
 		);
 	}
