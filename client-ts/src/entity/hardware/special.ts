873da834197cec227824acf93bb6dde127ef0fae
Russs <russs.khalilov@gmail.com>
2023-10-11 18:55:09 +0300

    h_11.10.2023

--- client-ts/src/entity/hardware/special.ts
+++ client-ts/src/entity/hardware/special.ts
@@ -1,109 +1,172 @@
-import { SpecialVehicleComponent } from '../specialVehicleComponent'
-import { Trucker } from '../cabins/rare'
-import { RacingWheelST, LandingGearST, RacingWheel, LandingGear } from '../movement/rare'
-import { LittleBoy6LB, DTCobra, AD12Falcon } from '../weapons/rare'
-import { R2Chill, AmmoPack, Blastoff, CSTaymyr, TS1Horizon, RD1Listener } from './rare'
-import * as HardwareImages from './HardwareImages'
-export class Chameleon extends SpecialVehicleComponent {
+import { SpecialVehicleComponent } from "../specialVehicleComponent";
+// import { Trucker } from "../cabins/rare";
+// import MovementRareArray, {
+//   RacingWheelST,
+//   LandingGearST,
+//   RacingWheel,
+//   LandingGear,
+// } from "../movement/rare";
+// import { LittleBoy6LB, DTCobra, AD12Falcon } from "../weapons/rare";
+// import HardwareRareArray, {
+//   R2Chill,
+//   AmmoPack,
+//   Blastoff,
+//   CSTaymyr,
+//   TS1Horizon,
+//   RD1Listener,
+// } from "./rare";
+import * as HardwareImages from "./HardwareImages";
+import CabinsCommonArray from "../cabins/common";
+import HardwareRareArray from "./rare";
+import MovementRareArray from "../movement/rare";
+import { RareVehicleComponent } from "../rareVehicleComponent";
+import { CommonVehicleComponent } from "../commonVehicleComponent";
+
+let Trucker: CommonVehicleComponent = CabinsCommonArray[2];
+let LittleBoy6LB: RareVehicleComponent = WeaponsRareArray[5];
+let AD12Falcon: RareVehicleComponent = WeaponsRareArray[9];
+let DTCobra: RareVehicleComponent = WeaponsRareArray[10];
+
+let [
+  StuddedWheel,
+  StuddedWheelST,
+  ChainedWheel,
+  ChainedWheelST,
+  BalloonTyre,
+  BalloonTyreST,
+  RacingWheel,
+  RacingWheelST,
+  LandingGear,
+  LandingGearST,
+  LargeWheel,
+  LargeWheelST,
+]: RareVehicleComponent[] = MovementRareArray;
+
+let [
+  FuelTank,
+  RD1Listener,
+  TS1Horizon,
+  BigG,
+  Blastoff,
+  R2Chill,
+  CSTaymyr,
+  AmmoPack,
+]: RareVehicleComponent[] = HardwareRareArray;
+
+class Chameleon extends SpecialVehicleComponent {
   constructor() {
-    super()
-    this.id = 315
-    this.name = 'Хамелеон'
-    this.type = 'Невидимость'
-    this.ingredients = [new LandingGear(), new CSTaymyr()]
-    this.img = HardwareImages.Chameleon
+    super();
+    this.id = 315;
+    this.name = "Хамелеон";
+    this.type = "Невидимость";
+    this.ingredients = [LandingGear, CSTaymyr];
+    this.img = HardwareImages.Chameleon;
   }
 }
-export class KA1Discharger extends SpecialVehicleComponent {
+class KA1Discharger extends SpecialVehicleComponent {
   constructor() {
-    super()
-    this.id = 316
-    this.name = 'КА-1 Разрядник'
-    this.type = 'Ускоритель перезарядки'
-    this.scrapMetal = 200
-    this.copper = 200
-    this.plastic = 30
-    this.ingredients = [new CSTaymyr(), new Trucker()]
-    this.img = HardwareImages.KA1Discharger
+    super();
+    this.id = 316;
+    this.name = "КА-1 Разрядник";
+    this.type = "Ускоритель перезарядки";
+    this.scrapMetal = 200;
+    this.copper = 200;
+    this.plastic = 30;
+    this.ingredients = [CSTaymyr, Trucker];
+    this.img = HardwareImages.KA1Discharger;
   }
 }
-export class OculusVI extends SpecialVehicleComponent {
+class OculusVI extends SpecialVehicleComponent {
   constructor() {
-    super()
-    this.id = 317
-    this.name = 'Окулус VI'
-    this.type = 'Радар'
-    this.ingredients = [new RD1Listener(), new TS1Horizon()]
-    this.img = HardwareImages.OculusVI
+    super();
+    this.id = 317;
+    this.name = "Окулус VI";
+    this.type = "Радар";
+    this.ingredients = [RD1Listener, TS1Horizon];
+    this.img = HardwareImages.OculusVI;
   }
 }
-export class Maxwell extends SpecialVehicleComponent {
+class Maxwell extends SpecialVehicleComponent {
   constructor() {
-    super()
-    this.id = 318
-    this.name = 'Максвелл'
-    this.type = 'Радар'
-    this.ingredients = [new LandingGearST(), new TS1Horizon()]
-    this.img = HardwareImages.Maxwell
+    super();
+    this.id = 318;
+    this.name = "Максвелл";
+    this.type = "Радар";
+    this.ingredients = [LandingGearST, TS1Horizon];
+    this.img = HardwareImages.Maxwell;
   }
 }
-export class PU1Charge extends SpecialVehicleComponent {
+class PU1Charge extends SpecialVehicleComponent {
   constructor() {
-    super()
-    this.id = 319
-    this.name = 'ЭУ-1 Заряд'
-    this.type = 'Генератор'
-    this.ingredients = [new DTCobra(), new TS1Horizon()]
-    this.img = HardwareImages.PU1Charge
+    super();
+    this.id = 319;
+    this.name = "ЭУ-1 Заряд";
+    this.type = "Генератор";
+    this.ingredients = [DTCobra, TS1Horizon];
+    this.img = HardwareImages.PU1Charge;
   }
 }
-export class Ampere extends SpecialVehicleComponent {
+class Ampere extends SpecialVehicleComponent {
   constructor() {
-    super()
-    this.id = 320
-    this.name = 'Ампер'
-    this.type = 'Генератор'
-    this.ingredients = [new RacingWheelST(), new R2Chill()]
-    this.img = HardwareImages.Ampere
+    super();
+    this.id = 320;
+    this.name = "Ампер";
+    this.type = "Генератор";
+    this.ingredients = [RacingWheelST, R2Chill];
+    this.img = HardwareImages.Ampere;
   }
 }
-export class Hardcore extends SpecialVehicleComponent {
+class Hardcore extends SpecialVehicleComponent {
   constructor() {
-    super()
-    this.id = 321
-    this.name = 'Жесткач'
-    this.type = 'Двигатель'
-    this.ingredients = [new Blastoff(), new CSTaymyr()]
-    this.img = HardwareImages.Hardcore
+    super();
+    this.id = 321;
+    this.name = "Жесткач";
+    this.type = "Двигатель";
+    this.ingredients = [Blastoff, CSTaymyr];
+    this.img = HardwareImages.Hardcore;
   }
 }
-export class Razorback extends SpecialVehicleComponent {
+class Razorback extends SpecialVehicleComponent {
   constructor() {
-    super()
-    this.id = 322
-    this.name = 'Работяга'
-    this.type = 'Двигатель'
-    this.ingredients = [new Trucker(), new LittleBoy6LB()]
-    this.img = HardwareImages.Razorback
+    super();
+    this.id = 322;
+    this.name = "Работяга";
+    this.type = "Двигатель";
+    this.ingredients = [Trucker, LittleBoy6LB];
+    this.img = HardwareImages.Razorback;
   }
 }
-export class DunHorse extends SpecialVehicleComponent {
+class DunHorse extends SpecialVehicleComponent {
   constructor() {
-    super()
-    this.id = 323
-    this.name = 'Саврас'
-    this.type = 'Двигатель'
-    this.ingredients = [new RacingWheel(), new R2Chill()]
-    this.img = HardwareImages.DunHorse
+    super();
+    this.id = 323;
+    this.name = "Саврас";
+    this.type = "Двигатель";
+    this.ingredients = [RacingWheel, R2Chill];
+    this.img = HardwareImages.DunHorse;
   }
 }
-export class Genesis extends SpecialVehicleComponent {
+class Genesis extends SpecialVehicleComponent {
   constructor() {
-    super()
-    this.id = 324
-    this.name = 'Генезис'
-    this.type = 'Боеприпасы'
-    this.ingredients = [new AD12Falcon(), new AmmoPack()]
-    this.img = HardwareImages.Genesis
+    super();
+    this.id = 324;
+    this.name = "Генезис";
+    this.type = "Боеприпасы";
+    this.ingredients = [AD12Falcon, AmmoPack];
+    this.img = HardwareImages.Genesis;
   }
 }
+
+const HardwareSpecialArray: SpecialVehicleComponent[] = [
+  new Chameleon(),
+  new KA1Discharger(),
+  new OculusVI(),
+  new Maxwell(),
+  new PU1Charge(),
+  new Ampere(),
+  new Hardcore(),
+  new Razorback(),
+  new DunHorse(),
+  new Genesis(),
+];
+export default HardwareSpecialArray;
