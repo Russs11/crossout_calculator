32dace868f5a503dc055c64d0cc28c0174d81914
Russs <russs.khalilov@gmail.com>
2023-10-11 18:28:46 +0300

    h_11.10.2023

--- client-ts/src/entity/cabins/special.ts
+++ client-ts/src/entity/cabins/special.ts
@@ -1,35 +1,51 @@
-import { SpecialVehicleComponent } from '../specialVehicleComponent'
-import { Trucker, Growl, Wyvern } from './rare'
-import { RD1Listener, BigG } from '../hardware/rare'
-import { AD12Falcon } from '../weapons/rare'
-import * as CabinsImages from './CabinsImages'
-export class Bat extends SpecialVehicleComponent {
+import { SpecialVehicleComponent } from "../specialVehicleComponent";
+// import { Trucker, Growl, Wyvern } from './rare'
+import CabinsRareArray from "./rare";
+// import { RD1Listener, BigG } from '../hardware/rare'
+import HardwareRareArray from "../hardware/rare";
+// import { AD12Falcon } from '../weapons/rare'
+import WeaponsRareArray from "../weapons/rare";
+import * as CabinsImages from "./CabinsImages";
+import { RareVehicleComponent } from "../rareVehicleComponent";
+
+let [Growl, Wyvern, Trucker]: RareVehicleComponent[] = CabinsRareArray;
+let AD12Falcon: RareVehicleComponent = WeaponsRareArray[9];
+let RD1Listener: RareVehicleComponent = HardwareRareArray[1];
+let BigG: RareVehicleComponent = HardwareRareArray[3];
+class Bat extends SpecialVehicleComponent {
   constructor() {
-    super()
-    this.id = 108
-    this.name = 'Нетопырь'
-    this.type = 'Легкая кабина'
-    this.ingredients = [new Growl(), new BigG()]
-    this.img = CabinsImages.Bat
+    super();
+    this.id = 108;
+    this.name = "Нетопырь";
+    this.type = "Легкая кабина";
+    this.ingredients = [Growl, BigG];
+    this.img = CabinsImages.Bat;
   }
 }
-export class Pilgrim extends SpecialVehicleComponent {
+class Pilgrim extends SpecialVehicleComponent {
   constructor() {
-    super()
-    this.id = 109
-    this.name = 'Пилигрим'
-    this.type = 'Средняя кабина'
-    this.ingredients = [new Wyvern(), new AD12Falcon()]
-    this.img = CabinsImages.Pilgrim
+    super();
+    this.id = 109;
+    this.name = "Пилигрим";
+    this.type = "Средняя кабина";
+    this.ingredients = [Wyvern, AD12Falcon];
+    this.img = CabinsImages.Pilgrim;
   }
 }
-export class Jawbreaker extends SpecialVehicleComponent {
+class Jawbreaker extends SpecialVehicleComponent {
   constructor() {
-    super()
-    this.id = 110
-    this.name = 'Зубодробитель'
-    this.type = 'Тяжелая кабина'
-    this.ingredients = [new Trucker(), new RD1Listener()]
-    this.img = CabinsImages.Jawbreaker
+    super();
+    this.id = 110;
+    this.name = "Зубодробитель";
+    this.type = "Тяжелая кабина";
+    this.ingredients = [Trucker, RD1Listener];
+    this.img = CabinsImages.Jawbreaker;
   }
 }
+const SpecialCabinsArray: SpecialVehicleComponent[] = [
+  new Bat(),
+  new Pilgrim(),
+  new Jawbreaker(),
+];
+
+export default SpecialCabinsArray;
